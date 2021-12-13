3dc44fc79620ea54a2980b92191aac78a8a82c56
MMMonir <helpinfosdesk@gmail.com>
2021-12-13 23:24:49 +0600

    Contact Email Update

--- js/email.js
+++ js/email.js
@@ -1,8 +1,11 @@
 function handleEmail(){
+    const status = document.getElementById("status");
+    status.innerHTML = "Email Sending.....";
     emailjs.init("user_yB7JbRwkUQ477ok1xjot9");
     const gmailServiceId = 'service_f2cncwj';
     const templateId = 'template_vr3d4pa';
 
+    const form = document.getElementById("formInputs")
     const name = document.getElementById("nameInput").value;
     const email = document.getElementById("emailInput").value;
     const subject = document.getElementById("subjectInput").value;
@@ -13,12 +16,13 @@ function handleEmail(){
     emailjs.send(gmailServiceId, templateId, emailDetails)
     .then(function(res){
         if(res.status === 200){
-            alert('Email Send Successfully');
-            document.getElementById("nameInput").value ='';
-            document.getElementById("emailInput").value ='';
-            document.getElementById("subjectInput").value ='';
-            document.getElementById("msgInput").value = '';
+            form.reset();
+            status.classList.add("success");
+            status.innerHTML = "Successfully Sended";
         }
-        else{ alert('There is error at sending message. ') }
-    }) 
+        else{ 
+            status.classList.add("error");
+            status.innerHTML = "Failed! Please try again";
+        }
+    })
 }
